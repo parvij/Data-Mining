4819b447f078e3853cd83bb3e2d910a4833c219c
parvij <parvij@gmail.com>
2019-11-24 19:00:29 -0500

    Create utility.py

---
+++ utility.py
@@ -0,0 +1,130 @@
+def resumetable(df):
+    print(f"Dataset Shape: {df.shape}")
+    summary = pd.DataFrame(df.dtypes,columns=['dtypes'])
+    summary = summary.reset_index()
+    summary['Name'] = summary['index']
+    summary = summary[['Name','dtypes']]
+    summary['Missing'] = df.isnull().sum().values    
+    summary['Uniques'] = df.nunique().values
+    summary['First Value'] = df.loc[0].values
+    summary['Second Value'] = df.loc[1].values
+    summary['Third Value'] = df.loc[2].values
+
+    for name in summary['Name'].value_counts().index:
+        summary.loc[summary['Name'] == name, 'Entropy'] = round(stats.entropy(df[name].value_counts(normalize=True), base=2),2) 
+
+    return summary
+    
+    
+    
+    ## Function to reduce the DF size
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2    
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)    
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+    
+    
+    
+    
+# visualization binary
+
+def vis_bin(df,cols):
+    #Looking the V's features
+    import matplotlib.gridspec as gridspec # to do the grid of plots
+    grid = gridspec.GridSpec(len(cols)/2, 2) # The grid of chart
+    plt.figure(figsize=(16,20)) # size of figure
+
+    # loop to get column and the count of plots
+    for n, col in enumerate(df[cols]): 
+        ax = plt.subplot(grid[n]) # feeding the figure of grid
+        sns.countplot(x=col, data=df, hue='target', palette='hls') 
+        ax.set_ylabel('Count', fontsize=15) # y axis label
+        ax.set_title(f'{col} Distribution by Target', fontsize=18) # title label
+        ax.set_xlabel(f'{col} values', fontsize=15) # x axis label
+        sizes=[] # Get highest values in y
+        for p in ax.patches: # loop to all objects
+            height = p.get_height()
+            sizes.append(height)
+            ax.text(p.get_x()+p.get_width()/2.,
+                    height + 3,
+                    '{:1.2f}%'.format(height/total*100),
+                    ha="center", fontsize=14) 
+        ax.set_ylim(0, max(sizes) * 1.15) # set y limit based on highest heights
+
+    plt.show()
+
+
+#visualization for category cols and binary target
+def ploting_cat_fet(df, cols, vis_row=5, vis_col=2,target='target'):
+    
+    grid = gridspec.GridSpec(vis_row,vis_col) # The grid of chart
+    plt.figure(figsize=(17, 35)) # size of figure
+
+    # loop to get column and the count of plots
+    for n, col in enumerate(df_train[cols]): 
+        tmp = pd.crosstab(df_train[col], df_train[target], normalize='index') * 100
+        tmp = tmp.reset_index()
+        tmp.rename(columns={0:'No',1:'Yes'}, inplace=True)
+
+        ax = plt.subplot(grid[n]) # feeding the figure of grid
+        sns.countplot(x=col, data=df_train, order=list(tmp[col].values) , color='green') 
+        ax.set_ylabel('Count', fontsize=15) # y axis label
+        ax.set_title(f'{col} Distribution by Target', fontsize=18) # title label
+        ax.set_xlabel(f'{col} values', fontsize=15) # x axis label
+
+        # twinX - to build a second yaxis
+        gt = ax.twinx()
+        gt = sns.pointplot(x=col, y='Yes', data=tmp,
+                           order=list(tmp[col].values),
+                           color='black', legend=False)
+        gt.set_ylim(tmp['Yes'].min()-5,tmp['Yes'].max()*1.1)
+        gt.set_ylabel("Target %True(1)", fontsize=16)
+        sizes=[] # Get highest values in y
+        for p in ax.patches: # loop to all objects
+            height = p.get_height()
+            sizes.append(height)
+            ax.text(p.get_x()+p.get_width()/2.,
+                    height + 3,
+                    '{:1.2f}%'.format(height/total*100),
+                    ha="center", fontsize=14) 
+        ax.set_ylim(0, max(sizes) * 1.15) # set y limit based on highest heights
+
+
+    plt.subplots_adjust(hspace = 0.5, wspace=.3)
+    plt.show()
+    
+    
+    
+    
+#prepareing ordinal feature    
+# Importing categorical options of pandas
+from pandas.api.types import CategoricalDtype 
+
+# seting the orders of our ordinal features
+ord_1 = CategoricalDtype(categories=['Novice', 'Contributor','Expert', 
+                                     'Master', 'Grandmaster'], ordered=True)
+ord_2 = CategoricalDtype(categories=['Freezing', 'Cold', 'Warm', 'Hot',
+                                     'Boiling Hot', 'Lava Hot'], ordered=True)
